74ce919b2b81096e711b57d6a3dd004fdec230c3
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-03-24 01:10:29 -0300

    Footer

--- wp-content/themes/cidade-viva-theme/footer.php
+++ wp-content/themes/cidade-viva-theme/footer.php
@@ -4,20 +4,34 @@
         	<div class="left">
             	<div id="link-footer">
                     <div class="links-footer">
-                        <ul>
+                        <?php
+						  wp_nav_menu( array('menu' => 'Menu Footer 1', 'container' => ''));
+					   	?> 
+						<?php /*?><ul>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
-                        </ul>
+                        </ul><?php */?>
                     </div>
                     <div class="links-footer">
-                        <ul>
+                        <?php
+						  wp_nav_menu( array('menu' => 'Menu Footer 2', 'container' => ''));
+						  echo('
+								<script type="text/javascript">									
+									$("#footer ul, #footer ul li, #footer ul li a").removeAttr("class").removeAttr("id");
+									$.map($("#footer ul li a"), function(t,index){
+										$(t).attr("class", "boldItalic11pt upperCase");
+									});
+								</script>
+							');
+					   	?> 
+						<?php /*?><ul>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
                             <li><a href="#" class="boldItalic11pt">Link 1</a></li>
-                        </ul>
+                        </ul><?php */?>
                     </div>
                 </div>
             </div>
