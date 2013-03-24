5d0e6a18f3c3a03ea7beefc39585dc8b52fb9149
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-03-23 22:18:33 -0300

    header

--- wp-content/themes/cidade-viva-theme/header.php
+++ wp-content/themes/cidade-viva-theme/header.php
@@ -11,7 +11,16 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <script src="<?php bloginfo( 'template_url' ) ?>/js/jquery.min.js" type="text/javascript"></script>
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); wp_head(); ?>
-
+<script>
+function verificatexto(campo) {
+	if (campo.value == "")
+		campo.value = "BUSCA";
+}
+function limpacampo(campo) {
+	if (campo.value == "BUSCA")
+		campo.value = "";
+}
+</script>
 </head>
 <body>
 <div id="corpo">
@@ -23,14 +32,36 @@
 			<?php wp_list_cats('sort_column=name'); ?>
 		</ul><?php */?>
         <div id="header-logo">
-            <div id="logo">
+            <div id="boxLogo" class="left">
             	<div>
-            		<img src="<?php bloginfo( 'template_url' ) ?>/images/logo.png" alt="Cidade Viva" />
+            		<a href="<?php get_home_url(); ?>">
+                    	<img src="<?php bloginfo( 'template_url' ) ?>/images/logo.png" alt="Cidade Viva" />
+                    </a>
                 </div>
             </div>
-            <div id="busca">
-                <div>Busca</div>
-                <div>Banner</div>
+            <div id="boxBusca" class="left">
+                <div id="busca">
+                	<div id="redesSociais">
+                    	<ul>
+                        	<li><a href="#" class="facebook"></a></li>
+                        	<li><a href="#" class="twitter"></a></li>
+                        	<li><a href="#" class="plus"></a></li>
+                        	<li><a href="#" class="instagram"></a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <form role="search" method="get" id="searchform" action="<?php get_home_url(); ?>">
+                            <div>
+                                <input type="text" value="BUSCA" name="s" id="s" onblur="verificatexto(this)" onfocus="limpacampo(this)">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div id="pub-Header">
+                	<a href="#">
+                    	<img src="<?php bloginfo( 'template_url' ) ?>/images/banner-header.png" />
+	                </a>
+                </div>
             </div>
         </div>
         
