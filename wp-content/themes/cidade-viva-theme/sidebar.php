d54347a345d90d1a60822ffb699453c2f655b0e5
Kevin Lira <lira.kevin@prox3.com.br>
2013-03-24 01:26:26 -0300

    Sidebar

--- wp-content/themes/cidade-viva-theme/sidebar.php
+++ wp-content/themes/cidade-viva-theme/sidebar.php
@@ -1,3 +1,7 @@
 		<div id="sidebar">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?><?php endif; ?>
+			 <?php if(function_exists( 'wp_bannerize' )) wp_bannerize(); ?>
+             
+<div class="sidebar-text">             CIDADE VIVA NO FACEBOOK</div>
+             <center>
+             <iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fprogramacidadeviva&amp;width=300&amp;height=258&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color=%23fff&amp;header=false&amp;appId=144045589086177" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:243px; height:258px;" allowTransparency="true"></iframe></center>
 		</div>
