6b2d04028aacbd23dfad56a35e6e69102a7d08f1
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-03-24 06:46:09 -0300

    Home

--- wp-content/themes/cidade-viva-theme/index.php
+++ wp-content/themes/cidade-viva-theme/index.php
@@ -1,42 +1,164 @@
 <?php get_header(); ?>
 	<div id="conteudo">
-    	<div id="destaques">
-        	<div id="destaquePrin" class="left">
-            <a href="#">
+    	<div id="page-content">
+        <div id="destaques">
+        	<?php
+				$args = array( 'numberposts' => '3', 'orderby' => 'post_date' );
+				$recent_posts = wp_get_recent_posts($args);
+			?>
+            <div id="destaquePrin" class="left">
+            <a href="<?= get_permalink($recent_posts[0]["ID"]) ?>" title="Look <?= esc_attr($recent_posts[0]["post_title"])?> ">
+	            <div class="playIconDest1"></div>
             	<div class="imgDestaquePrin">
-                	<img src="<?php bloginfo( 'template_url' ) ?>/images/foto_entrevista.jpg" width="100%" height="100%"/>
+                	<?php /*?><img src="<?php get_the_post_thumbnail($recent_posts[0]["ID"]) ?>" width="100%" height="100%"/><?php */?>
+                    <?= get_the_post_thumbnail($recent_posts[0]["ID"]) ?>
                 </div>
                 <div class="tituloDestaque">
-                	<p class="upperCase">PROGRAMA CIDADE VIVA INÉDITO DIA 00/00</p>
+                	<p class="upperCase"><?= $recent_posts[0]["post_title"] ?></p>
                 </div>
             </a>
             </div>
             <div id="destaqueSegun" class="right">
+            	<div>
+                    <a href="<?= get_permalink($recent_posts[1]["ID"]) ?>" title="Look <?= esc_attr($recent_posts[1]["post_title"])?> ">
+                    <div class="playIconDest2"></div>
+                    <div class="imgDestaquePrin">
+                        <?= get_the_post_thumbnail($recent_posts[1]["ID"]) ?>
+                    </div>
+                    <div class="tituloDestaque">
+                        <p class="upperCase"><?= $recent_posts[1]["post_title"] ?></p>
+                    </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="<?= get_permalink($recent_posts[2]["ID"]) ?>" title="Look <?= esc_attr($recent_posts[2]["post_title"])?> ">
+                    <div class="playIconDest2"></div>
+                    <div class="imgDestaquePrin">
+                        <?= get_the_post_thumbnail($recent_posts[2]["ID"]) ?>
+                    </div>
+                    <div class="tituloDestaque">
+                        <p class="upperCase"><?= $recent_posts[2]["post_title"] ?></p>
+                    </div>
+                    </a>
+                </div>
             </div>
         </div>
-		<?php /*?><div id="artigos">
-		
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<div class="artigo">
-					<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-					<p>Postado por <?php the_author() ?> em <?php the_time('d/M/Y') ?> - <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?> <?php edit_post_link('(Editar)'); ?></p>
-					<p><?php the_content(); ?></p>
-				</div>			
-			<?php endwhile?>
-				<div class="navegacao">
-					<div class="recentes"><?php next_posts_link('&laquo; Artigos Anteriores') ?></div>
-					<div class="anteriores"><?php previous_posts_link('Artigos Recentes &raquo;') ?></div>
-				</div>
-			<?php else: ?>
-				<div class="artigo">
-					<h2>Nada Encontrado</h2>
-					<p>Erro 404</p>
-					<p>Lamentamos mas não foram encontrados artigos.</p>
-				</div>			
-			<?php endif; ?>
-			
-		</div><?php */?>
-		
+        <?php
+			echo('
+				<script>
+				$("#destaques img").attr({ \'width\': \'100%\', \'height\':\'100%\' });
+				</script>
+			');
+		?>
+        <div id="programacao-completa">
+        	<div class="icon-Programacao"></div>
+            <div class="content-Programacao">
+            <ul>
+            	<li>
+                	<a href="#">
+                		<div class="box-Programacao" style="border-right:3px solid #000;">
+                            <div class="centerVerticalAlign" style="border-right:0px;">
+                                <p class="boldItalic11ptBlack">00:00</p>
+                                <p class="boldItalic11ptBlack upperCase">no ar</p>
+                            </div>
+                            <div class="centerVerticalAlign">
+                                <p class="italic11ptGray">Descricao</p>
+                            </div>
+                        </div>
+    	            </a>
+                </li>
+                <li>
+                	<a href="#">
+                    	<div class="box-Programacao" style="border-right:3px solid #000;">
+                            <div class="centerVerticalAlign" style="border-right:0px;">
+                                <p class="boldItalic11ptBlack">00:00</p>
+                                <p class="boldItalic11ptBlack upperCase">reprise</p>
+                            </div>
+                            <div class="centerVerticalAlign">
+                                <p class="italic11ptGray">Descricao</p>
+                            </div>
+                        </div>
+    	            </a>
+                </li>
+                <li>
+                	<a href="#">
+                    	<div class="box-Programacao">
+                            <div class="centerVerticalAlign" style="border-right:0px;">
+                                <p class="boldItalic11ptBlack">00:00</p>
+                                <p class="boldItalic11ptBlack upperCase">reprise</p>
+                            </div>
+                            <div class="centerVerticalAlign">
+                                <p class="italic11ptGray">Descricao</p>
+                            </div>
+                        </div>
+    	            </a>
+                </li>
+            </ul>
+            </div>
+            <div class="list-Programacao">
+            	<a href="#" class="centerVerticalAlign boldItalic11ptBlack">
+                	programação completa
+            	</a>
+            </div>
+        </div>
+        
+        <div id="extras-Programacao">
+        	<ul>
+            	<li>
+                    <a href="#">
+                        <div class="titulo-extras upperCase boldItalic11pt">revistas</div>
+                        <div class="img-extras">
+                            <div class="revistaIcon"></div>
+                            <img src="<?php bloginfo( 'template_url' ) ?>/images/revista.jpg" width="100%" height="100%" />
+                        </div>
+                    </a>
+                </li>
+            	<li>
+                    <a href="#">
+                        <div class="titulo-extras upperCase boldItalic11pt">coluna no jornal</div>
+                        <div class="img-extras">
+                            <div class="jornalIcon"></div>
+                            <img src="<?php bloginfo( 'template_url' ) ?>/images/jornal.jpg" width="100%" height="100%" />
+                        </div>
+                    </a>
+                </li>
+            	<li>
+                <div style="display: inline-table;">
+                        <div class="titulo-extras upperCase boldItalic11pt">novidades no blog</div>
+					<a href="#">
+                        <div class="img-extras-blog">
+                            <img src="<?php bloginfo( 'template_url' ) ?>/images/blog1.jpg" width="100%" height="100%" />
+                        </div>
+                        <div>
+                        <p class="upperCase boldItalic11ptBlack">
+                        ACONTECEU NO PROGRAMA 
+                        </p>
+                        <p class="italic11ptGray">
+                        Você fica sabendo de vários assuntos
+                        </p>
+                        </div>
+                        <hr class="linhCinza" />
+                    </a>
+                    <a href="#">
+                        <div class="img-extras-blog">
+                            <img src="<?php bloginfo( 'template_url' ) ?>/images/blog2.jpg" width="100%" height="100%" />
+                        </div>
+                        <div>
+                        <p class="upperCase boldItalic11ptBlack">
+                        ACONTECEU NO PROGRAMA 
+                        </p>
+                        <p class="italic11ptGray">
+                        Você fica sabendo de vários assuntos
+                        </p>
+                        </div>
+						<hr class="linhCinza" />
+                    </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        
+        </div>
 		<?php get_sidebar(); ?>
 	</div>
 <?php get_footer(); ?>
