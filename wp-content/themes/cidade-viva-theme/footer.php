1d56fbdae02b2ebc1f67281a0be8486cdcb78a35
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-03-24 00:46:33 -0300

    footer

--- wp-content/themes/cidade-viva-theme/footer.php
+++ wp-content/themes/cidade-viva-theme/footer.php
@@ -1,8 +1,33 @@
 	<div id="footer">
 		<div id="background-footer"></div>
         <div id="content">
-        	<div class="left">Menu</div>
-        	<div class="right">Actmob</div>
+        	<div class="left">
+            	<div id="link-footer">
+                    <div class="links-footer">
+                        <ul>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                        </ul>
+                    </div>
+                    <div class="links-footer">
+                        <ul>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                            <li><a href="#" class="boldItalic11pt">Link 1</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        	<div class="right">
+    	        <div id="contentAct">
+            		<a href="http://actmob.com/" target="_blank">
+                    	<img src="<?php bloginfo( 'template_url' ) ?>/images/actmob.png" alt="ActMob" />
+	        	    </a>
+        	    </div>
+            </div>
         </div>
 	</div>
 </div>
