33679b384a49970076b938756b328cf71c12445c
DennerLuan <denner.luan@prox3.com.br>
2013-03-25 22:55:21 -0300

    mundaças na archive

--- wp-content/themes/cidade-viva-theme/archive.php
+++ wp-content/themes/cidade-viva-theme/archive.php
@@ -8,21 +8,7 @@
 				include("blog_page.php");
 			}else{
 		?>
-        
-			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-			<?php /* If this is a category archive */ if (is_category()) { ?>
-				Arquivo da Categoria "<?php echo single_cat_title(); ?>"
-			<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				Arquivo de <?php the_time('j \d\e F \d\e Y'); ?>
-			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				Arquivo de <?php the_time('F \d\e Y'); ?>
-			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				Arquivo de <?php the_time('Y'); ?>
-			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				Arquivo do Autor
-			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				Arquivo do Blog
-			<?php } ?>
+       
 			
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<div class="artigo">
--- wp-content/themes/cidade-viva-theme/revista_page.php
+++ wp-content/themes/cidade-viva-theme/revista_page.php
@@ -1,9 +1,14 @@
-
+<style type="text/css">
+	.left{float:left !important} .right{float:right !important}
+	
+	.title_edition{width:100%; height:auto; font-family:Unbuntu; font-size:14px; color:#555555; font-style:italic}
+	.title_edition:hover{text-decoration:underline}
+	
+	.body_edition img{width:150px; height:195px; float:left; display:block}
+</style>
 <?php if ( have_posts() ) : while ( have_posts() ) :  the_post(); ?>
 	<div class="body_edition left">
-  	<a href="<?php the_permalink() ?>" class="title_edition" style="width:100%; height:auto; font-family:Unbuntu; font-size:14px; color:#555555; font-style:italic">
-			<?php the_title(); ?>
-    </a>
+  	<a href="<?php the_permalink() ?>" class="title_edition left"><?php the_title(); ?></a>
   	<?php the_post_thumbnail(); ?>
   </div>
 <?php endwhile?>
