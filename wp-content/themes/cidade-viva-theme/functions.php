64a3c202a885c4cf29ab32862b26340f2945800a
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-03-26 00:53:25 -0300

    Functions e Style

--- wp-content/themes/cidade-viva-theme/functions.php
+++ wp-content/themes/cidade-viva-theme/functions.php
@@ -33,52 +33,70 @@ function loopPost($atts, $content = null) {
 	}
 	$wp_query->query($query);
 	ob_start();
+	
+	$catId = get_term_by('name', $category, 'category')->term_id;
 	?>
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ) ?>/css/blog.css" />
-	<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-      <div class="artigo blog" style="margin-bottom:5px; width:710px;">
-          <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-          <div class="infoBlog">
-              <div class="calendar"></div>
-              <div class="date">
-                  <p style="text-transform:capitalize;"><?php the_time('d M, Y') ?></p>
+        <?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
+			if($catId ==  get_term_by('name', 'blog', 'category')->term_id) {
+		?>
+              <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ) ?>/css/blog.css" />    
+              <div class="artigo blog" style="margin-bottom:5px; width:710px;">
+                  <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                  <div class="infoBlog">
+                      <div class="calendar"></div>
+                      <div class="date">
+                          <p style="text-transform:capitalize;"><?php the_time('d M, Y') ?></p>
+                      </div>
+                  </div>
+                  <div class="infoContent">
+                      <div>
+                        <?php
+                            if ( has_post_thumbnail() )
+                                the_post_thumbnail();
+                            echo('
+                              <script type="text/javascript">
+                                  $(".infoContent img").removeAttr("width").removeAttr("height");
+                                  $.map($(".infoContent img"), function(t,index){
+                                      $(t).attr("width", "100%").attr("height","200");
+                                  });
+                              </script>
+                            ');
+                        ?>
+                      </div>
+                      <div class="content">
+                          <p><?php the_excerpt(); ?></p>
+                      </div>
+                      <div class="linkLeiaMais">
+                      <a href="<?php the_permalink() ?>">Leia Mais...</a>
+                      </div>
+                      <div class="footer">
+                          <p>Postado por <?php the_author() ?></p>
+                      </div>
+                  </div>
+                  <hr style="border:0px; border-top: 1px solid #eee;" />
               </div>
-          </div>
-          <div class="infoContent">
-          	  <div>
-              	<?php
-              		if ( has_post_thumbnail() )
-						the_post_thumbnail();
-					echo('
-					  <script type="text/javascript">
-						  $(".infoContent img").removeAttr("width").removeAttr("height");
-						  $.map($(".infoContent img"), function(t,index){
-							  $(t).attr("width", "100%").attr("height","200");
-						  });
-					  </script>
-					');
-				?>
-              </div>
-              <div class="content">
-                  <p><?php the_excerpt(); ?></p>
-              </div>
-              <div class="linkLeiaMais">
-              <a href="<?php the_permalink() ?>">Leia Mais...</a>
-              </div>
-              <div class="footer">
-                  <p>Postado por <?php the_author() ?></p>
-              </div>
-          </div>
-          <hr style="border:0px; border-top: 1px solid #eee;" />
-      </div>
-	<?php endwhile; ?>
-    
-	<?php if(pagination == 'true'){ ?>
-	<div class="navigation">
-	  <div class="alignleft"><?php previous_posts_link('« Antigos') ?></div>
-	  <div class="alignright"><?php next_posts_link('Próximos »') ?></div>
-	</div>
-	<?php } ?>
+ 		<?php    } 
+			else if($catId ==  (get_term_by('name', 'revista', 'category')->term_id || get_term_by('name', 'coluna no jornal', 'category')->term_id)) { ?>
+    		<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ) ?>/css/revista-jornal.css" />  
+            <div class="body_edition left">
+            	<a href="<?php the_permalink() ?>">
+            		<span class="title_edition left"><?php the_title(); ?></span>
+            		<?php the_post_thumbnail(); ?>
+            	</a>
+            </div>
+		<?php 
+        }
+        ?>
+        <?php endwhile; ?>
+        
+        <?php if(pagination == 'true'){ ?>
+        <div class="navigation">
+          <div class="alignleft"><?php previous_posts_link('« Antigos') ?></div>
+          <div class="alignright"><?php next_posts_link('Próximos »') ?></div>
+        </div>
+        <?php 
+        }
+	?>
 	<?php $wp_query = null; $wp_query = $temp;
 	$content = ob_get_contents();
 	ob_end_clean();
