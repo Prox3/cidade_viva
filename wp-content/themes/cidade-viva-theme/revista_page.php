d1c37ee461ee5b6d3745716d6f79ef8515cc5cbd
DennerLuan <denner.luan@prox3.com.br>
2013-03-26 00:21:47 -0300

    apply css in unique archive

--- wp-content/themes/cidade-viva-theme/revista_page.php
+++ wp-content/themes/cidade-viva-theme/revista_page.php
@@ -1,12 +1,3 @@
-<style type="text/css">
-	.left{float:left !important} .right{float:right !important}
-	
-	.title_edition{width:100%; height:auto; font-family:Unbuntu; font-size:14px; color:#555555; font-style:italic; font-weight:bold}
-	.title_edition:hover{text-decoration:underline}
-	
-	.body_edition img{width:150px; height:195px; float:left; display:block}
-	.body_edition{margin-right:15px}
-</style>
 <?php if ( have_posts() ) : while ( have_posts() ) :  the_post(); ?>
 	<div class="body_edition left">
   	<a href="<?php the_permalink() ?>">
