f1971c3dd489615817b9988635d8c57a9b501083
DennerLuan <denner.luan@prox3.com.br>
2013-03-25 23:42:52 -0300

    Single.php

--- wp-content/themes/cidade-viva-theme/revista_page.php
+++ wp-content/themes/cidade-viva-theme/revista_page.php
@@ -1,15 +1,18 @@
 <style type="text/css">
 	.left{float:left !important} .right{float:right !important}
 	
-	.title_edition{width:100%; height:auto; font-family:Unbuntu; font-size:14px; color:#555555; font-style:italic}
+	.title_edition{width:100%; height:auto; font-family:Unbuntu; font-size:14px; color:#555555; font-style:italic; font-weight:bold}
 	.title_edition:hover{text-decoration:underline}
 	
 	.body_edition img{width:150px; height:195px; float:left; display:block}
+	.body_edition{margin-right:15px}
 </style>
 <?php if ( have_posts() ) : while ( have_posts() ) :  the_post(); ?>
 	<div class="body_edition left">
-  	<a href="<?php the_permalink() ?>" class="title_edition left"><?php the_title(); ?></a>
-  	<?php the_post_thumbnail(); ?>
+  	<a href="<?php the_permalink() ?>">
+			<span class="title_edition left"><?php the_title(); ?></span>
+  		<?php the_post_thumbnail(); ?>
+    </a>
   </div>
 <?php endwhile?>
 <?php endif; ?>
--- wp-content/themes/cidade-viva-theme/single.php
+++ wp-content/themes/cidade-viva-theme/single.php
@@ -1,11 +1,23 @@
+<style type="text/css">
+	.left{float:left !important} .right{float:right !important}
+	
+	.title_post{width:100%; height:auto; font-family:Unbuntu; font-size:22px; color:#555555; font-weight:bold}
+	.title_edition:hover{text-decoration:underline}
+	
+	.info_date_post{width:100%; height:auto; font-family:Unbuntu; font-size:12px; color:#555555; font-style:italic; margin:5px 0 5px 0}
+	
+	.body_edition img{width:150px; height:195px; float:left; display:block}
+	.body_edition{margin-right:15px}
+</style>
 <?php get_header(); ?>
 	<div id="conteudo">
 		<div id="artigos">
 		
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<div class="artigo">
-					<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-					<p>Postado por <?php the_author() ?> em <?php the_time('d/M/Y') ?> - <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?> <?php edit_post_link('(Editar)'); ?></p>
+					<span class="title_post left"><?php the_title(); ?></span>
+					<span class="info_date_post left">Postado por <?php the_author() ?> em <?php the_time('d/M/Y') ?></span>
+          <span class='st_facebook_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='facebook'></span><span class='st_twitter_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='twitter'></span><span class='st_email_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='email'></span><span class='st_sharethis_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='sharethis'></span><span class='st_fblike_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='fblike'></span><span class='st_plusone_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='plusone'></span><span class='st_pinterest_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='pinterest'></span><span class='st_instagram_buttons' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='instagram'></span>
 					<p><?php the_content(); ?></p>
 				</div>
 				
